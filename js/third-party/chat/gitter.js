ec03a8e5f5cf04b201b98bb5fa2ed506b8dc24c0
willkyu <z496373158@163.com>
2023-07-07 19:18:18 +0800

    Site updated: 2023-07-07 19:18:16

--- js/third-party/chat/gitter.js
+++
@@ -1,7 +0,0 @@
-/* global CONFIG */
-
-((window.gitter = {}).chat = {}).options = {
-  room: CONFIG.gitter.room
-};
-
-<script src="https://sidecar.gitter.im/dist/sidecar.v1.js" async defer></script>
